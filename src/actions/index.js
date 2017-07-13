35e44ca853a445c2d6f39b790ddf91197eaa71bc
alantrinh <alan.l.trinh@gmail.com>
2017-07-13 21:57:13 +0200

    completed redux weather chart

---
+++ src/actions/index.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+const API_KEY = "a8b5bdb44c85ba9f0722e338db190ee1";
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?APPID=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+    const url = `${ROOT_URL}&q=${city},us`;
+    const request = axios.get(url);
+
+    return {
+        type: FETCH_WEATHER,
+        payload: request
+    };
+}
